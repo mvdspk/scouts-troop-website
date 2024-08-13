9c2859ab6d12453d6e38062b83b751d208fedf38
Pavan Kumar, Malyala <123mvds@gmail.com>
2024-08-13 14:45:26 -0700

    nav

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-function showSection(){
-    document.querySelectorAll(".section").forEach(section => {
-        console.log(section.id)
-    })
+function showSection(name) {
+  document.querySelectorAll(".section").forEach((section) => {
+    if (section.id === name) {
+      console.log(section.id);
+      section.classList.remove("d-none");
+    } else {
+      section.classList.add("d-none");
+    }
+  });
 }
 
+function handleNavSelection(e) {
+    console.log(e)
+  let sectionName = e.target.id.split("-")[0];
+  showSection(sectionName);
 
-document.querySelector("body").onload = () =>{
-    showSection()
+  highlightNavItem(e.target.id);
 }
+
+function highlightNavItem(name) {
+  document.querySelectorAll(".nav-element").forEach((section) => {
+    if (section.id === name) {
+      console.log(section.id);
+      section.classList.add("nav-selected");
+    } else {
+      section.classList.remove("nav-selected");
+    }
+  });
+}
+
+document.querySelector("body").onload = () => {
+  showSection("home");
+};
